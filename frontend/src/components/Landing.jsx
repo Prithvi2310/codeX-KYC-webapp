3914da5a310c15f2d7de110c300bbfe19230cb36
Akash Panda <aakipandap@gmail.com>
2024-03-13 22:41:22 +0530

    landing page

---
+++ frontend/src/components/Landing.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar/Navbar';
+import Hero from './Hero/Hero';
+
+const Landing = () => {
+  return (
+    <>
+        <Navbar />
+        <Hero />
+    </>
+  )
+}
+
+export default Landing
